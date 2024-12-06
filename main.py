cf126e7a2ef47dee0efe33cd86c69d2a2e510fad
Logan Hufstetler <logan.hufstetler@gmail.com>
2024-12-06 10:26:09 -0800

    get AD Username from the command line arguments. No check atm.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from BeyondTrust.auth.BTauth import BT_get_token
 from BeyondTrust.newUser.user import create_user
@@ -7,8 +8,13 @@ from config.user import get_ADUser
 # Load environment variables
 load_dotenv()
 
+# Load command line arguments
+cmd_arg = sys.argv[1]
+
+# Load the new hire that needs access to Help Desk programs including their name and email. Username is also attached.
+new_hire = get_ADUser(cmd_arg)
+
 # BTaccess_token = BT_get_token()["access_token"]
 
 # create_user(BTaccess_token, "HelpDesk Test", "HelpDeskTest@ensignservices.net")
-
-get_ADUser(123742866)
+# create_user(BTaccess_token, new_hire.name, new_hire.email)
