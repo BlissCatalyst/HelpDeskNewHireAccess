0f2216425f3dade98317f8e506a754d1c4675e63
Logan Hufstetler <logan.hufstetler@gmail.com>
2024-12-12 13:20:03 -0800

    Added a way to list all of the roles for our domain. Need to translate ID numbers.

--- GoogleAdmin/newUser/user.py
+++ GoogleAdmin/newUser/user.py
@@ -1,14 +1,35 @@
+import pprint
+import json
 from GoogleAdmin.auth.auth import create_google_credentials
 import googleapiclient.discovery
-import json
 
-def get_user(newUser):
+def get_user(user_id):
     credentials = create_google_credentials()
 
     googleadmin_service = googleapiclient.discovery.build('admin', 'directory_v1', credentials=credentials)
 
-    user_info = googleadmin_service.users().get(userKey=newUser.email).execute()
+    user_info = googleadmin_service.users().get(userKey=user_id).execute()
 
     print(f"User info: {json.dumps(user_info)}")
+
+    return
+
+def get_user_role_assignments(user_id):
+    credentials = create_google_credentials()
+
+    googleadmin_service = googleapiclient.discovery.build("admin", "directory_v1", credentials=credentials)
+
+    # user_role_info = googleadmin_service.customer().get()
+
+    return
+
+def get_role_assignment_list(customer_id):
+    credentials = create_google_credentials()
+
+    googleadmin_service = googleapiclient.discovery.build("admin", "directory_v1", credentials=credentials)
+
+    role_assignment_list = googleadmin_service.roleAssignments().list(customer=customer_id).execute()
+
+    pprint.pprint(role_assignment_list)
 
     return
--- main.py
+++ main.py
@@ -5,20 +5,22 @@ from config.user import get_ADUser
 from BeyondTrust.auth.BTauth import BT_get_token
 from BeyondTrust.newUser.user import create_user
 
-from GoogleAdmin.newUser.user import get_user
+from GoogleAdmin.newUser.user import get_user, get_role_assignment_list
 
 # Load environment variables
 load_dotenv()
+GOOGLE_CUSTOMER_ID = os.getenv("GOOGLE_CUSTOMER_ID")
 
 # Load command line arguments
-cmd_arg = sys.argv[1]
+# cmd_arg = sys.argv[1]
 
 # Load the new hire that needs access to Help Desk programs including their name and email. Username is also attached.
-new_hire = get_ADUser(cmd_arg)
+# new_hire = get_ADUser(cmd_arg)
 
 # BTaccess_token = BT_get_token()["access_token"]
 
 # create_user(BTaccess_token, "HelpDesk Test", "HelpDeskTest@ensignservices.net")
 # create_user(BTaccess_token, new_hire.name, new_hire.email)
 
-get_user(new_hire)
+# get_user(new_hire)
+get_role_assignment_list(GOOGLE_CUSTOMER_ID)
